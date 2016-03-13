2f0280e05039e5e10629fb2a7d75abe02fc090a0
Alireza Mirzaeiyan <alireza.mirzaeiyan@yahoo.com>
2016-03-13 13:01:24 +0330

    Add option parser

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 __author__ = 'Alireza Mirzaeiyan'
 
+import optparse
 from nqueens import NQueens
 
 if __name__ == '__main__':
-    nqueens = NQueens(dimension=8, population_count=500, mutation_factor=0.2, iteration=2000000)
+    parser = optparse.OptionParser()
+    parser.add_option('-d', dest='dimension', type='int', metavar="Dimension")
+    parser.add_option('-p', dest='population_count', type='int', metavar="Population Count")
+    parser.add_option('-m', dest='mutation_factor', type='float', metavar="Mutation Factor")
+    parser.add_option('-i', dest='iteration', type='int', metavar="Iteration")
+    (options, args) = parser.parse_args()
+
+    nqueens = NQueens(options.dimension, options.population_count, options.mutation_factor, options.iteration)
     nqueens.solve()
